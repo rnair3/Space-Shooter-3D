9dcbeb288c604b8c80527d3aac5abe4c85d00cea
Rinita Nair <rnair3@uncc.edu>
2019-11-01 16:37:36 -0400

    Designing Terrain and Spleash Screen

---
+++ Argon Assault/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("m/s")][SerializeField] float xSpeed = 4f;
+    [Tooltip("m")] [SerializeField] float xRange = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = horizontalThrow * xSpeed * Time.deltaTime;
+
+        transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange), transform.localPosition.y, transform.localPosition.z);
+    }
+}
